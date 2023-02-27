29ff9d4a2c42024ba342d37194e484ab117c6005
Hubert Au <hubertau@tutanota.com>
2023-02-27 14:23:17 +0000

    first restructure

--- haystacks_expanded/utils/test_pyktok.py
+++
@@ -1,6 +0,0 @@
-import pyktok as pyk
-
-pyk.save_tiktok('https://www.tiktok.com/@tiktok/video/7194841883313491242?is_copy_url=1&is_from_webapp=v1',
-	        True,
-                'video_data.csv',
-		'firefox')
---
+++ tests/test_search.py
@@ -0,0 +1,17 @@
+import subprocess
+import glob
+import os
+import pytest
+
+def test_search(tmp_path):
+    result = subprocess.run(['hay', 'search', 'health', '--savepath', tmp_path], capture_output=True)
+    assert b"200" in result.stderr
+
+def test_one_download(tmp_path):
+    # get most recent json file
+    files = glob.glob(os.path.join('./../data/01_raw', '*.json'))
+    files = sorted(files, key=os.path.getmtime, reverse=True)
+    file = files[0]
+    result = subprocess.run(['hay', 'download', file, '--savepath', tmp_path,   '--max-download', '2'])
+    downloaded_files = glob.glob(tmp_path, '*.mp4')
+    assert len(downloaded_files) == 2
