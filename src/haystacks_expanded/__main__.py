b98041ad5807b2c377bba2194f0f3017d67e3a5b
Hubert Au <hubertau@tutanota.com>
2023-10-11 14:18:24 +0100

    idiotic error correction

--- src/haystacks_expanded/__main__.py
+++ src/haystacks_expanded/__main__.py
@@ -361,10 +361,10 @@ def splitdata(ctx, data, base_model, model_type, outfile = None, max_len = 128):
 def train(ctx, data, model_type, output_dir, base_model, batch_size, resume, num_train_epochs, early_stopping_patience):
 
     # Convert input to a boolean if it's "true" or "false"
-    if my_option.lower() == 'true':
-        my_option = True
-    elif my_option.lower() == 'false':
-        my_option = False
+    if resume.lower() == 'true':
+        resume = True
+    elif resume.lower() == 'false':
+        resume = False
 
     if model_type == 'LLM':
         main.train_model(
